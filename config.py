d2764c32f0c4730064d141e9b45e90d24a7f9dee
pr13260 <67130996+pr13260@users.noreply.github.com>
2021-01-21 03:16:15 +0530

    Update

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 TOKEN = os.environ.get("BOT_TOKEN")
-START_MESSAGE = os.environ.get("START_MESSAGE", "*Hi ! I am a simple torrent searcher using @sjprojects's Torrent Searcher api.\n\n\nMade with 🐍 by @KeralasBots*")
-FOOTER_TEXT = os.environ.get("FOOTER_TEXT", "*Made with ❤️ by @KeralasBots*")
+START_MESSAGE = os.environ.get("START_MESSAGE", "*Hi ! I am a simple torrent searcher using Torrent Searcher api.\n\n\nMade in 🐍*")
+FOOTER_TEXT = os.environ.get("FOOTER_TEXT", "*Made with ❤️ by @thestarkarmy13260*")
